9088406c4194f9849b54d10c797e6c7060590613
Shaurya0987 <shaurya3537@gmail.com>
2025-11-24 21:54:51 +0530

    okkk2

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Studifyy",
+      title: "Studify",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade800),
